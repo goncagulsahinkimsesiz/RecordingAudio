44a80143127d95f46f174f01ce9b509788e30be4
Goncagul Sahin Kimsesiz <goncagulshn1@gmail.com>
2025-01-06 16:44:20 +0300

    deneme added

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ seconds = 3  # Duration of recording
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
 sd.wait()  # Wait until recording is finished
 write('output.wav', fs, myrecording)  # Save as WAV file
+
+#deneme
